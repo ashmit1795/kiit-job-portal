ba603840b64592a84f759aebcdb77a1822d6cadd
ashmit1795 <ashmitpatra77@gmail.com>
2026-02-25 22:55:34 +0530

    feat: initialize server and app setup with error handling

---
+++ app/backend/src/server.js
@@ -0,0 +1,32 @@
+import http from "http";
+import app from "./app.js";
+import env from "./config/env.js";
+import logger from "./utils/logger.js";
+
+const server = http.createServer(app);
+
+server.listen(env.PORT, () => {
+	logger.info("Server started successfully", {
+		port: env.PORT,
+        environment: env.NODE_ENV,
+        url: `http://localhost:${env.PORT}/`
+	});
+});
+
+process.on("unhandledRejection", (err) => {
+	logger.error("Unhandled Rejection occurred", {
+		message: err.message,
+		stack: err.stack,
+	});
+
+	process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+	logger.error("Uncaught Exception occurred", {
+		message: err.message,
+		stack: err.stack,
+	});
+
+	process.exit(1);
+});
