bde3fdea73c136e1ade5d4597bd4322f876e88a7
ashmit1795 <ashmitpatra77@gmail.com>
2026-03-03 12:32:53 +0530

    fix: simplify response data in AuthController by returning the user object directly

--- app/backend/src/modules/users/auth.controller.js
+++ app/backend/src/modules/users/auth.controller.js
@@ -6,17 +6,9 @@ class AuthController {
 		try {
 			const user = await userService.syncUser(req.user);
 
-			const responseUser = {
-				id: user.id,
-				email: user.email,
-				roll_number: user.roll_number,
-				role: user.role,
-                profile_completed: user.profile_completed
-			};
-
 			return new AppResponse({
 				message: "User authenticated successfully",
-				data: responseUser,
+				data: user,
 			}).send(res);
 		} catch (err) {
 			next(err);
