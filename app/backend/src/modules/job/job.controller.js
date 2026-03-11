e92c39e3f1fa471ccd469c8449758014d0129632
ashmit1795 <ashmitpatra77@gmail.com>
2026-03-11 17:54:59 +0530

    feat: implement JobController with methods for job creation, retrieval, approval, and rejection

---
+++ app/backend/src/modules/job/job.controller.js
@@ -0,0 +1,73 @@
+import AppResponse from "../../utils/AppResponse.js";
+import jobService from "./job.service.js";
+
+class JobController {
+	async createJob(req, res, next) {
+		try {
+			const job = await jobService.createJob(req.user, req.body, req.file);
+
+			return new AppResponse({
+				message: "Job created successfully",
+				data: job,
+			}).send(res);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async getJobs(req, res, next) {
+		try {
+			const jobs = await jobService.getJobs(req.user);
+
+			return new AppResponse({
+				message: "Jobs fetched successfully",
+				data: jobs,
+			}).send(res);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async getJobById(req, res, next) {
+		try {
+			const job = await jobService.getJobById(req.params.id);
+
+			return new AppResponse({
+				message: "Job details fetched successfully",
+				data: job,
+			}).send(res);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async approveJob(req, res, next) {
+		try {
+			const job = await jobService.approveJob(req.user, req.params.id);
+
+			return new AppResponse({
+				message: "Job approved successfully",
+				data: job,
+			}).send(res);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async rejectJob(req, res, next) {
+		try {
+			const job = await jobService.rejectJob(req.user, req.params.id);
+
+			return new AppResponse({
+				message: "Job rejected successfully",
+				data: job,
+			}).send(res);
+		} catch (err) {
+			next(err);
+		}
+	}
+}
+
+const jobController = new JobController();
+
+export default jobController;
